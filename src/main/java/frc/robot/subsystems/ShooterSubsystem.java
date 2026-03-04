0db171f26288b249e0d443fde800d64f2a438be3
frc10553 <oregonhsrobotics@gmail.com>
2026-03-04 17:34:54 -0600

    shooter PID

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -16,12 +18,24 @@ public class ShooterSubsystem implements Subsystem {
     // constructor
     public ShooterSubsystem() {
         motor = new TalonFX(Constants.CanIDs.SHOOTER_LEFT_MOTOR);
-        hood = new SparkMax(Constants.CanIDs.SHOOTER_HOOD, MotorType.kBrushless);
+        var slot0Configs = new Slot0Configs();
+        slot0Configs.kS = 0; // Add 0.1 V output to overcome static friction
+        slot0Configs.kV = 0; // A velocity target of 1 rps results in 0.12 V output
+        slot0Configs.kP = SmartDashboard.getNumber("Shooter Arm P", 0); // An error of 1 rps results in 0.11 V output
+        slot0Configs.kI = SmartDashboard.getNumber("Shooter Arm I", 0); // no output for integrated error
+        slot0Configs.kD = SmartDashboard.getNumber("Shooter Arm D", 0); // no output for error derivative
+
+        motor.getConfigurator().apply(slot0Configs);
+
+         hood = new SparkMax(Constants.CanIDs.SHOOTER_HOOD, MotorType.kBrushless);
         SmartDashboard.putNumber("Shooter Speed", this.defaultSpeed);
     }
 
     public void startMotor() {
-        motor.set(SmartDashboard.getNumber("Shooter Speed", this.defaultSpeed));
+        // motor.set(SmartDashboard.getNumber("Shooter Speed", this.defaultSpeed));
+
+        final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
+        motor.setControl(m_request.withVelocity(SmartDashboard.getNumber("Shooter RPS", 100)));
     }
 
     public void hoodPosition(Constants.HoodPositions hoodPosition) {
