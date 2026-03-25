2af465267260d14d3c0b34e1b4331470512de50a
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2026-03-24 19:50:25 -0500

    change to extend SubsystemBase, add periodic()s, and remove setDefaultCommands

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -10,10 +10,10 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class IntakeSubsystem implements Subsystem {
+public class IntakeSubsystem extends SubsystemBase {
     private final TalonFX armMotor;
     private final SparkMax intakeWheelMotor;
 
