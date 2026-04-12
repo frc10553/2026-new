bc943fe774975cb61dc1ae4ce1761bd5aac79c3f
frc10553 <oregonhsrobotics@gmail.com>
2026-04-12 17:51:28 -0500

    ShooterSubsystem Changes

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -3,15 +3,21 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.CoastOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -23,7 +29,16 @@ public class ShooterSubsystem extends SubsystemBase {
     private final TalonFX leftMotor;
     private final TalonFX rightMotor;
     private final SparkMax feeder;
-    private final double defaultSpeed = 0.9;
+    //private final double defaultSpeed = 0.9;
+    // private static double kMaxVelocity = 1.75;
+    // private static double kMaxAcceleration = 0.75;
+    // private static final double kP = 1.3;
+    // private static final double kI = 0.0;
+    // private static double kD = 0.7;
+    // private double targetRPM = 0;
+
+    // private final PIDController leftController = new PIDController(kP, kI, kD);
+
 
     // constructor
     public ShooterSubsystem() {
@@ -31,34 +46,73 @@ public class ShooterSubsystem extends SubsystemBase {
         leftMotor = new TalonFX(Constants.CanIDs.SHOOTER_LEFT_MOTOR);
         rightMotor = new TalonFX(Constants.CanIDs.SHOOTER_RIGHT_MOTOR);
         feeder = new SparkMax(Constants.CanIDs.SHOOTER_FEEDER, MotorType.kBrushless);
+        
+        // leftController.setTolerance(100);
+        
+        var motorOutputConfigs = new MotorOutputConfigs();
+        motorOutputConfigs.Inverted = InvertedValue.CounterClockwise_Positive;
+        motorOutputConfigs.NeutralMode = NeutralModeValue.Coast;
+
+        var invertedMotorOutputConfigs = new MotorOutputConfigs();
+        invertedMotorOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
+        invertedMotorOutputConfigs.NeutralMode = NeutralModeValue.Coast;
 
         var slot0Configs = new Slot0Configs();
         slot0Configs.kS = 0;
-        slot0Configs.kV = 0;
-        slot0Configs.kP = 2;
-        slot0Configs.kI = 1.93;
-        slot0Configs.kD = 0.18;
+        slot0Configs.kV = 0.2;
+        slot0Configs.kP = 3;
+        //1.93
+        slot0Configs.kI = 0;
+        //0.18
+        slot0Configs.kD = 0.35;
 
         var currentLimits = new CurrentLimitsConfigs();
         currentLimits.SupplyCurrentLimit = 60;
         currentLimits.StatorCurrentLimit = 100;
 
-        rightMotor.getConfigurator().apply(slot0Configs);
-        rightMotor.getConfigurator().apply(currentLimits);
+        var configs = new TalonFXConfiguration();
+        configs.Slot0 = slot0Configs;
+        configs.MotorOutput = motorOutputConfigs;
+        configs.CurrentLimits = currentLimits;
 
-
-        leftMotor.setControl(new Follower(rightMotor.getDeviceID(), MotorAlignmentValue.Opposed));
-
+        rightMotor.getConfigurator().apply(configs);
+        leftMotor.getConfigurator().apply(configs.withMotorOutput(invertedMotorOutputConfigs));
+        
+        leftMotor.setControl(new CoastOut());
         rightMotor.setControl(new CoastOut());
 
 
         SmartDashboard.putNumber("activeShooterVelocity", rightMotor.getVelocity().getValueAsDouble());
+        SmartDashboard.putNumber("Shooter RPS", 60);
     }
 
     public void startMotor(boolean feeding) {
-        final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
-        rightMotor.setControl(m_request.withVelocity(feeding ? -100 : -SmartDashboard.getNumber("Shooter RPS", 80)));
+        if(feeding){
+            rightMotor.setControl(new VelocityVoltage(-100));
+            leftMotor.setControl(new VelocityVoltage(-100));
+
+        }else{
+            leftMotor.setControl(new VelocityVoltage(-SmartDashboard.getNumber("Shooter RPS", 60)));
+            rightMotor.setControl(new VelocityVoltage(-SmartDashboard.getNumber("Shooter RPS", 60)));
+        }
     }
+
+    // public void setTargetRPM(double rpm){
+    //     targetRPM = rpm;
+    // }
+
+    // public void stop(){
+    //     targetRPM = 0;
+    // }
+
+    // public double getCurrentRPM(){
+    //     // TODO get actual encoder value
+    //     return 0;
+    // }
+
+    // public boolean atSpeed(){
+    //     return Math.abs(getCurrentRPM()-targetRPM)<100;
+    // }
 
     public void startFeeder(boolean crossFielding) {
         feeder.setVoltage(crossFielding ? -7 : -8);
@@ -69,7 +123,8 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public void stopMotor() {
-        rightMotor.set(0);
+        leftMotor.setControl(new CoastOut());
+        rightMotor.setControl(new CoastOut());
         feeder.set(0);
     }
 
@@ -143,7 +198,12 @@ public class ShooterSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("activeShooterVelocity", rightMotor.getVelocity().getValueAsDouble());
+        SmartDashboard.putNumber("activeShooterVelocityRight", rightMotor.getVelocity().getValueAsDouble());
+        SmartDashboard.putNumber("activeShooterVelocityLeft", leftMotor.getVelocity().getValueAsDouble());
+        // double currentRPM = getCurrentRPM();
+        // double output = leftController.calculate(currentRPM, targetRPM);
+        // output = Math.max(-1, Math.min(1, output));
+        // System.out.println(targetRPM + currentRPM + output);
     }
 
     public boolean isConnected() {
