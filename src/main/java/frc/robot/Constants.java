4a747883e3f471f7248f68b7c1daad551dec4ed9
frc10553 <oregonhsrobotics@gmail.com>
2026-04-29 15:19:30 -0500

    formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 public class Constants {
     public static final class CanIDs {
         public static final int SHOOTER_LEFT_MOTOR = 30;
@@ -15,6 +17,32 @@ public class Constants {
 
         public static final int INTAKE_ARM = 36;
         public static final int INTAKE_WHEELS = 37;
+
+
+
+        
+        //Arm Constants
+        public static final int kMotorPort = 0;
+        public static final int kEncoderAChannel = 0;
+        public static final int kEncoderBChannel = 1;
+        public static final int kJoystickPort = 0;
+
+        public static final String kArmPositionKey = "ArmPosition";
+        public static final String kArmPKey = "ArmP";
+
+  // The P gain for the PID controller that drives this arm.
+        public static final double kDefaultArmKp = 50.0;
+        public static final double kDefaultArmSetpointDegrees = 75.0;
+
+  // distance per pulse = (angle per revolution) / (pulses per revolution)
+  //  = (2 * PI rads) / (4096 pulses)
+        public static final double kArmEncoderDistPerPulse = 2.0 * Math.PI / 4096;
+
+        public static final double kArmReduction = 200;
+        public static final double kArmMass = 8.0; // Kilograms
+        public static final double kArmLength = Units.inchesToMeters(30);
+        public static final double kMinAngleRads = Units.degreesToRadians(-75);
+        public static final double kMaxAngleRads = Units.degreesToRadians(255);
     }
 
     // public static final double HOOD_GEAR_RATIO = 5.0 * 5.0 * (40 / 15); // 5:1 x 5:1 gearbox + 40/15 tooth gear ratio
